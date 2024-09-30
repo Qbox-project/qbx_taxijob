9313ac2dea8a3cb60a2ca159b0313bea9c9b3fc7
Swellington Soares <qb.corebr@gmail.com>
2024-09-30 07:27:00 -0300

    remove old language module and add pt-br language

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,10 +7,7 @@ version '1.0.0'
 
 shared_scripts {
 	'@ox_lib/init.lua',
-	'@qbx_core/modules/lib.lua',
-	'@qbx_core/shared/locale.lua',
-	'locales/en.lua',
-	'locales/*.lua'
+	'@qbx_core/modules/lib.lua'	
 }
 
 client_script {
@@ -35,5 +32,5 @@ files {
 provide 'qb-taxijob'
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
-
+ox_lib 'locale'
 dependency 'qbx_core'
